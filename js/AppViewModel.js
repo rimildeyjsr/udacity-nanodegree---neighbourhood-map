ab6ca6a4f3dedc71091609012d3393da040baab0
rimildeyjsr <rimildeyjsr@gmail.com>
2016-10-25 23:00:34 +0530

    first knockout code working, show less show more toggling

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -1,5 +1,13 @@
 function AppViewModel (){
-
+    this.SomeValue = ko.observable("Show Less");
+    this.ShowLessMore = function(){
+        if (this.SomeValue() == "Show Less"){
+            this.SomeValue("Show More");
+        }
+        else if(this.SomeValue() == "Show More"){
+            this.SomeValue("Show Less");
+        }
+    };
 }
 
 // Activates knockout.js
